59efa8959e4d604e92301072580f74fbf74c0338
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2017-12-20 23:41:24 +0000

    Updated analysis

--- model_backend.py
+++ model_backend.py
@@ -7,15 +7,17 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 from requests.auth import HTTPBasicAuth
 import localsettings as settings
 
-api_root = 'http://localhost:8000/api/'
+api_root = 'http://medmod.kidzinski.com/api/'
 
 while(1):
-    r = requests.get(api_root + 'submissions/dequeue/knee/', auth=HTTPBasicAuth('admin', settings.PASS))
-#    print r.text
+    r = requests.get(api_root + 'submissions/dequeue/?model=knee', auth=HTTPBasicAuth('admin', settings.PASS))
+
     if len(r.text) < 10:
         time.sleep(5)
         continue
 
+    print(r)
+    print(r.text)
     toprocess = r.json()
 
     # if toprocess['status'] == 'empty':
@@ -29,19 +31,20 @@ while(1):
 
     print(toprocess['video'])
     urllib.urlretrieve (toprocess['video'], filename)
-    os.system("./process.sh %s %s" % (filename, submission_id)) 
+    # os.system("./process.sh %s %s" % (filename, submission_id)) 
 
-    analysis = None
-    analysis_file = "analysis/%s/analysis.json" % submission_id
-    if os.path.exists(analysis_file):
-        with open(analysis_file, 'r') as data_file:
-            analysis = data_file.read()
-
+    # analysis = None
+    # analysis_file = "analysis/%s/analysis.json" % submission_id
+    # if os.path.exists(analysis_file):
+    #     with open(analysis_file, 'r') as data_file:
+    #         analysis = data_file.read()
+    analysis = "test"
+    
     url = api_root + "submissions/%s/" % submission_id
 
     processed_video = None
     if analysis:
-        processed_video = (submission_id + ".mp4", open('downloads/%s.mp4' % submission_id,'rb'), 'video/mp4')
+        processed_video = (submission_id + ".jpg", open(filename), 'image/jpeg')
     fields = {
         'processed_video': processed_video,
         'analysis': analysis,

c792c905fcdbbb31604314284c965772640c704e
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2018-05-13 23:29:44 +0000

    Fix dirs

--- process.sh
+++ process.sh
@@ -1,9 +1,9 @@
-rm ../test_code/knee_OA_staging/data/test_images/*
-cp ${1} ../test_code/knee_OA_staging/data/test_images/
-cd ../test_code/knee_OA_staging
+rm ../knee_OA_staging/data/test_images/*
+cp ${1} ../knee_OA_staging/data/test_images/
+cd ../knee_OA_staging
 
 PRED=`python tools/demo_all_predictions.py --cpu --model MLP | tail -n1`
 
-cp data/output_test_images/* ../../model-worker/downloads/
-echo "$PRED" > ../../model-worker/downloads/${2}.txt
+cp data/output_test_images/* ../model-worker/downloads/
+echo "$PRED" > ../model-worker/downloads/${2}.txt
 
